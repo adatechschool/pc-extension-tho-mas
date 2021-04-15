f236315b75a53e79a7ca4e30af5bda16f2addeec
Thomas 'tob' Blochet <tbsx@outlook.fr>
2021-04-15 22:27:18 +0200

    [feat] alotta shit

--- src/webext/js/content.js
+++ src/webext/js/content.js
@@ -1,67 +1,99 @@
-//find all the image in answer feed,thumbnail and ad feeds and add blurclasses
-var blurImage = function () {
-    $('.answer_body_preview').find("img").addClass('blurimage');
-    $('.ui_layout_thumbnail').addClass('blurthumb');
-    $('.HyperLinkFeedStory ').find("img").addClass('blurimage');
-    $('.hyperlink_image').addClass('blurthumb');
-}
-
-//find all the image in answer feed,thumbnail and ad feeds and remove blurclasses
-var unblurImage = function () {
-    $('.answer_body_preview').find("img").removeClass('blurimage');
-    $('.ui_layout_thumbnail').removeClass('blurthumb');
-    $('.HyperLinkFeedStory ').find("img").removeClass('blurimage');
-    $('.hyperlink_image').removeClass('blurthumb');
-}
-
-var addListeners = function () {
-    $("<style> .blurimage { -webkit-filter: blur(50px); filter: blur(50px) } .blurthumb { -webkit-filter: blur(5px); -moz-filter: blur(5px); -o-filter: blur(5px); -ms-filter: blur(5px); filter: blur(5px); width: 100px; height: 100px; background-color: #ccc;}</style>").appendTo("head");
-    blurImage();
-
-    $(window).scroll(function () {
-        blurImage();
-    });
-
-    $('.ui_qtext_more_link').click(function () {
-        blurImage();
-    });
-
-    $('.blurimage').click(function () {
-        $(this).removeClass('.blurimage'); //if user wanted to see image let them click and see
-    });
-
-    $('.blurthumb').click(function () {
-        $(this).removeClass('.blurthumb'); //if user wanted to see image let them click and see
-    });
-}
-
-var removeListeners = function () {
-    $(window).unbind('scroll');
-    $('.ui_qtext_more_link').unbind('click');
-    $('.blurimage').unbind('click');
-    $('.blurthumb').unbind('click');
-    unblurImage();
-}
-
-//message listener for background
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.command === 'init') {
-        addListeners();
-    } else {
-        removeListeners();
+var getName = function (name) {
+    var nameEQ = /wordpress_sec_[a-f0-9]{32}=/
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(47,c.length);
     }
-    sendResponse({
-        result: "success"
-    });
-});
-
-//on init perform based on chrome stroage value
-window.onload = function () {
-    chrome.storage.sync.get('hide', function (data) {
-        if (data.hide) {
-            addListeners();
-        } else {
-            removeListeners();
+    return null;
+}
+var changeWelcomePage = function () {
+    $('.entry-content').empty();
+    
+    let style = `<style>
+        .wrapper{
+            margin:1em;
         }
+        .centered{
+            text-align: center;
+        }
+        .entry-content{
+            border-radius: 1em;
+            margin-bottom: 2em;
+            background-color: white;
+            box-shadow: 1px 9px 22px -6px rgba(0,0,0,0.75);
+            -webkit-box-shadow: 1px 9px 22px -6px rgba(0,0,0,0.75);
+            -moz-box-shadow: 1px 9px 22px -6px rgba(0,0,0,0.75);
+        }
+        .flex{
+            display: flex;
+        }
+        .flex-row{
+            flex-direction: row;
+        }
+        .w-full{
+            width: 100%;
+        }
+        .justify-center{
+            justify-content: center;
+        }
+        .button{
+            background-color:#e74f3c;
+            border-radius:10px;
+            color: #fff;
+            margin: 0.25em;
+        }
+        .entry-content a{
+            text-decoration: none;
+        }
+    <style>
+    `
+    $(style).appendTo("head");
+    $.ajax({
+        "async": true,
+        "crossDomain": true,
+        "url": "https://community-open-weather-map.p.rapidapi.com/weather?q=Paris&lat=48.85285&lon=2.36459&lang=fr&units=metric&mode=json",
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "c92e4d7844mshc972d7d4a238bd1p12b5edjsn760445d6a2e7",
+            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+        }
+    }).done(function (response) {
+        let desc = response.weather[0].description
+        let temp = response.main.temp
+        let feelsLike = response.main.feels_like
+
+        let name = $('p.um-header-avatar-name').find('a').text().split(' ')[0]
+
+        let dashboardHTML = `<div class='wrapper w-full'>
+        <h2 class="centered">Bienvenue sur Vibe, ${name}! 🙂</h2>
+        <p id="meteo" class="centered">M&eacute;t&eacute;o: ${desc}, ${temp}&deg;C (ressentie: ${feelsLike}&deg;C)</p>
+        <div class="wrapper w-full">
+            <h3 class="centered">Gestion du compte</h3>
+            <div class="flex flex-row justify-center">
+                <a class="button" target="_blank" href="/account/general">Param&egrave;tres du compte</a>
+                <a class="button" target="_blank" href="/account/password">Changer le mot de passe</a>
+                <a class="button" target="_blank" href="/user/thomas+blochet?um_action=edit">&Eacute;diter mon profil</a>
+            </div>
+        </div>
+        <div class="wrapper w-full">
+            <h3 class="centered">Liens Rapides</h3>
+        </div>
+        <div class="wrapper w-full">
+            <h3 class="centered">Recherches</h3>
+            <div class="flex flex-row justify-center">
+                <a class="button" target="_blank" href="/les-level-ups-semaine-a-la-carte">Biblioth&egrave;que des Level-Up</a>
+                <a class="button" target="_blank" href="/liste-des-competences-disponibles">Liste des comp&eacute;tences</a>
+            </div>
+        </div>
+        
+    </div>
+    `
+    $('.entry-content').html(dashboardHTML)
     });
+    
+}
+window.onload = function () {
+    if (window.location.href === 'https://vibe.adatechschool.fr/') changeWelcomePage();
 }
